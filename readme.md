04683cdb262f4b18655c42d1efe7e9b922f09620
韩春华 <hanchunhua@corp-ci.com>
2019-12-31 14:27:45 +0800

    添加readme

---
+++ readme.md
@@ -0,0 +1,10 @@
+
+
+# 安装依赖
+yarn install
+
+# 运行
+yarn watch
+
+# 打包
+yarn build
--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,13 @@ import routing from './routes/';
 import * as path from 'path';
 import * as bodyParse from 'koa-bodyparser';
 import * as staticFiles from 'koa-static';
+import * as cors from '@koa/cors';
 import { config } from './config';
 
 const app = new Koa();
+
+// 如果需要提供跨域资源配置下面的cors
+/* app.use(cors()); */
 
 app.use(bodyParse());
 
